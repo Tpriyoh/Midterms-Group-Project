fbf2b4741a54df4fe05db24def8a772a1bab5696
Tpriyoh <29980049+Tpriyoh@users.noreply.github.com>
2026-03-20 10:33:32 -0400

    Use stringstream for formatted transaction history
    Refactor deposit and withdraw methods to use stringstream for formatted output.

--- BankAccount.h
+++ BankAccount.h
@@ -10,6 +10,7 @@ Midterms Group Project
 #include <vector>
 #include <iomanip>
 #include <string>
+#include <sstream> //need this for stringstream.
 using namespace std;
 
 class BankAccount {
@@ -27,9 +28,19 @@ public:
      ownerName = n;
      accountNumber = an;
      balance = b;
+     
+     
+     //source for stringstream number rounding
+     //https://stackoverflow.com/questions/5113221/decimal-points-with-stdstringstream where one comment said: 
+     //"You don't need to split out the precision call on a separate line: t << setprecision(2) << fixed << a ..."
+     //used this to round up all string numbers to the second decimal place. will note the site only for the following refrences.
+     //rnd short for round.
+     stringstream rnd;
+     rnd << fixed << setprecision(2) << b;
+     
      //using + to_string() multiple times throughout the code to convert numbers into string to use for history
      //since the vector is a string type.
-     history.push_back("Account created with an initial deposit of: $" + to_string (b));
+     history.push_back("Account created with an initial deposit of: $" + rnd.str());
     
     }
     
@@ -39,7 +50,11 @@ public:
         // if amt is greater then 0 for the deposit this will trigger.
         if(amt > 0) {
             balance += amt;
-            history.push_back("Deposited: $" + to_string(amt));
+          
+          //source //https://stackoverflow.com/questions/5113221/decimal-points-with-stdstringstream *see note above  
+            stringstream rnd;
+            rnd << fixed << setprecision(2) << amt;
+            history.push_back("Deposited: $" + rnd.str());
         }
         //otherwise this will post saying your amount is invalid. wont show in history
         else {
@@ -66,7 +81,12 @@ public:
         //if the first 2 fail this will trigger taking your requested amt out of bal and adding that to the history. 
         else {
             balance -= amt;
-            history.push_back("Withdrawn: $" + to_string(amt));
+            
+            
+            //source //https://stackoverflow.com/questions/5113221/decimal-points-with-stdstringstream *see note above  
+            stringstream rnd;
+            rnd << fixed << setprecision(2) << amt;
+            history.push_back("Withdrawn: $" + rnd.str());
             
             //and if balance is 0 this message will alert the user of that.
             if(balance == 0) {
@@ -107,6 +127,7 @@ public:
             //used auto i(0) at first but that caused an unexpected behavior due to auto i(0) being treated as an int 
             // while history.size() is looked at as size_t that conflicting data type may have made other bugs. switched it to size_t i(0)
             //unsolved issue- when converting double to string fixed << setprecision(2) no longer works so the output will look like 100.000000000 in Transaction History.
+            //fixed issue above using stackoverflow guide
             for(size_t i(0); i < history.size(); ++i) {
                 cout << history[i] << endl;
             }
@@ -122,6 +143,8 @@ at first I only used push_back() as a placeholder forgetting to add the users en
 This gave me another problem seeing how im using a string type vector with an int or double number. I then looked online 
 at https://www.geeksforgeeks.org/cpp/stdto_string-in-cpp/
 to find the to_string function which made it easier to convert numbers to strings allowing it to be stored in that vector<string> type.
+//note: 3/20/26 using to_string will remove setprecision and make the output look like 100.00000000 in the history section.
+//I ended up using stackoverflow to find a solution that would let me round up string numbers which lead me to use stringstream.
 Another issue I made for myself is for void deposit, and withdrawl I had different paramaterized parameter 
 names like wthdraw or depo which confused me when i tried to make the if else statements because I kept 
 mixing up what var names belonged to which functions. as a result i just changed all of them to amt which not only 
